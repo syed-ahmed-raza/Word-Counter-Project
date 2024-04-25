f830e93c4d85a8421dcadebcc39ec1f9ba61240c
syed-ahmed-raza <ar03110386073@gmail.com>
2024-04-25 11:52:08 +0500

    Created Word Converter

--- word.js
+++ word.js
@@ -4,9 +4,9 @@ const answers = await inquirer.prompt([
     {
         name: "sentence",
         type: "input",
-        message: "Enter Your Sentence To Count The Word"
+        message: "Enter Your Sentence To Count The Word:"
     }
 ]);
-const words = answers.sentence.trim().split("");
+const words = answers.sentence.trim().split(" ");
 console.log(words);
 console.log(`Your Sentence Word Count Is ${words.length}`);
