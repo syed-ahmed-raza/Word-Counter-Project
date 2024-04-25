e34e5ed45a9f9fa5977778dc3c586f800794660e
syed-ahmed-raza <ar03110386073@gmail.com>
2024-04-25 11:47:12 +0500

    Created Words Counter

---
+++ word.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence To Count The Word"
+    }
+]);
+const words = answers.sentence.trim().split("");
+console.log(words);
+console.log(`Your Sentence Word Count Is ${words.length}`);
---
+++ word.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answers: {
+    sentence: string 
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter Your Sentence To Count The Word"
+    }
+])
+
+const words = answers.sentence.trim().split("")
+
+console.log(words)
+
+console.log(`Your Sentence Word Count Is ${words.length}`);
